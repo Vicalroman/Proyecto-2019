8a6740f5f0dc0d575675d63f0ce0a1f4fbcc4b81
Vicalroman <51000187+Vicalroman@users.noreply.github.com>
2019-08-12 00:01:24 -0400

    Producto de matrices en 1 linea agregado

--- Algebra.py
+++ Algebra.py
@@ -18,3 +18,14 @@ def mulMatrix(a,b):
     except TypeError:
         None
     return lista
+def mulMatrix_2(a,b):
+    try:
+        if len(a[0]) == len(b):
+            m_1 = len(a) #Filas de a
+            m_2 = len(b) #Filas de b
+            n_1 = len(a[0]) #Columnas de a
+            n_2 = len(b[0]) #Columnas de b
+            lista = [[sum([a[i][k] * b[k][j] for k in range(n_1)]) for j in range(m_1)]for i in range(n_2)]
+    except TypeError:
+        None
+    return lista
