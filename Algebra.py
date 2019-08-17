39dd6b34a3e4e0c66738bc55a3bfd5349212eb04
Vicalroman <51000187+Vicalroman@users.noreply.github.com>
2019-08-17 12:56:14 -0400

    Inversion por Gauss-Jordan hecho.

--- Algebra.py
+++ Algebra.py
@@ -29,3 +29,40 @@ def mulMatrix_2(a,b):
     except TypeError:
         None
     return lista
+def GaussJordan(a, b):
+    """DocString"""
+    a = [i for i in a]
+    b = [i for i in b]
+    n = len(a)
+    p = len(b[0])
+    det = 1
+    for i in range(n - 1):
+        k = i
+        for j in range(i + 1, n):
+            if abs(a[j][i]) > abs(a[k][i]):
+                k = j
+        if k != i:
+            a[i], a[k] = a[k], a[i]
+            b[i], b[k] = b[k], b[i]
+            det = -det
+
+        for j in range(i + 1, n):
+            t = a[j][i]/a[i][i]
+            for k in range(i + 1, n):
+                a[j][k] -= t*a[i][k]
+            for k in range(p):
+                b[j][k] -= t*b[i][k]
+
+    for i in range(n - 1, -1, -1):
+        for j in range(i + 1, n):
+            t = a[i][j]
+            for k in range(p):
+                b[i][k] -= t*b[j][k]
+        t = 1/a[i][i]
+        det *= a[i][i]
+        for j in range(p):
+            b[i][j] *= t
+    return b
+a = [[2,1],[5,3]]
+b = [[1,0],[0,1]]
+print(GaussJordan(a,b))
