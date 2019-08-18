bf674245500271cd21b266d5dcbe8e036002791f
Vicalroman <51000187+Vicalroman@users.noreply.github.com>
2019-08-18 13:59:19 -0400

    Arreglos

--- Algebra.py
+++ Algebra.py
@@ -19,6 +19,7 @@ def mulMatrix(a,b):
         None
     return lista
 def mulMatrix_2(a,b):
+    """DocString"""
     try:
         if len(a[0]) == len(b):
             m_1 = len(a) #Filas de a
@@ -29,37 +30,36 @@ def mulMatrix_2(a,b):
     except TypeError:
         None
     return lista
-def GaussJordan(a, b):
+def GaussJordan(a,b):
     """DocString"""
-    a = [i for i in a]
-    b = [i for i in b]
-    n = len(a)
-    p = len(b[0])
+    mat = [i for i in a] #
+    ident = [[1 if i == j else 0 for i in range(b)] for j in range(b)]
+    fil = len(mat)
+    col = len(ident[0])
     det = 1
-    for i in range(n - 1):
+    for i in range(col - 1):
         k = i
-        for j in range(i + 1, n):
-            if abs(a[j][i]) > abs(a[k][i]):
+        for j in range(i + 1, fil):
+            if abs(mat[j][i]) > abs(mat[k][i]):
                 k = j
         if k != i:
-            a[i], a[k] = a[k], a[i]
-            b[i], b[k] = b[k], b[i]
+            mat[i], mat[k] = mat[k] , mat[i]
+            ident[i], ident[k] = ident[k], ident[i]
             det = -det
-
-        for j in range(i + 1, n):
-            t = a[j][i]/a[i][i]
-            for k in range(i + 1, n):
-                a[j][k] -= t*a[i][k]
-            for k in range(p):
-                b[j][k] -= t*b[i][k]
-
-    for i in range(n - 1, -1, -1):
-        for j in range(i + 1, n):
-            t = a[i][j]
-            for k in range(p):
-                b[i][k] -= t*b[j][k]
-        t = 1/a[i][i]
+        for j in range(i + 1, fil):
+            t = mat[j][i]/mat[i][i]
+            for k in range(i + 1, fil):
+                mat[j][k] -= t*mat[i][k]
+            for k in range(col):
+                ident[j][k] -= t*ident[i][k]
+    for i in range(fil - 1, -1, -1):
+        for j in range(i + 1, fil):
+            t = mat[i][j]
+            for k in range(col):
+                ident[i][k] -= t*ident[j][k]
+        t = 1/mat[i][i]
         det *= a[i][i]
-        for j in range(p):
-            b[i][j] *= t
-    return b
+        for j in range(col):
+            ident[i][j] *= t
+    return ident
+print(GaussJordan([[2,1],[5,3]],2))
