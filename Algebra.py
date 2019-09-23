c6f578c6030d2d79c02e6699c59c2da5b8e4a786
Vicalroman <51000187+Vicalroman@users.noreply.github.com>
2019-09-23 01:29:01 -0400

    DocString actualization

--- Algebra.py
+++ Algebra.py
@@ -1,5 +1,8 @@
 def matrixProduct(a,b):
-    """DocString"""
+    """La funcion matrixProduct recibe 2 argumentos de tipo lista los cuale
+    simulen matrices, y calcula el producto entre esas dos matrices en el caso
+    que cumplan la condicion, y retorna una lista simulando la matriz resultante
+    del producto."""
     if len(a[0]) == len(b):
         row_1 = len(a) #Filas de a
         row_2 = len(b) #Filas de b
@@ -16,7 +19,10 @@ def matrixProduct(a,b):
             product.append(aux_list)
     return product
 def matrixProduct_2(a,b):
-    """DocString"""
+    """La funcion matrixProduct recibe 2 argumentos de tipo lista los cuale
+    simulen matrices, y calcula el producto entre esas dos matrices en el caso
+    que cumplan la condicion, y retorna una lista simulando la matriz resultante
+    del producto."""
     if len(a[0]) == len(b):
         row_1 = len(a) #Filas de a
         row_2 = len(b) #Filas de b
@@ -25,7 +31,8 @@ def matrixProduct_2(a,b):
         product = [[sum([a[i][k] * b[k][j] for k in range(row_1)]) for j in range(column_2)]for i in range(row_1)]
     return product
 def gaussJordan(a):
-    """DocString"""
+    """La funcion gaussJordan recibe 1 argumento del tipo lista, que recrea una matriz,
+    la funcion determina y retorna la inversa de la matriz indicada."""
     if len(a) == len(a[0]):
         mat = [i for i in a] #
         ident = [[1 if i == j else 0 for i in range(len(a))] for j in range(len(a))]
@@ -61,7 +68,8 @@ def gaussJordan(a):
                 ident[j][i] = round(ident[j][i] , 2)
         return ident
 def vectorProduct(a,b):
-    """DocString"""
+    """La funcion vectorProduct recibe 2 argumentos tipo lista que simulan vectores
+    y determina y retorna el resultado del producto vectorial de los dos vectores"""
     vector_1 = [i for i in a]
     vector_2 = [i for i in b]
     vector_r = []
@@ -74,7 +82,7 @@ def vectorProduct(a,b):
     vector_r = list(reversed(vector_r))
     return vector_r
 def transposition(a):
-    """DocString"""
+    """Recibe un argumento tipo lista que simule una matriz, y retorna su matriz transpuesta"""
     matr = [i for i in a]
     rows = len(matr)
     column = len(matr[0])
@@ -86,21 +94,24 @@ def transposition(a):
         trans.append(aux)
     return trans
 def transposition_2(a):
-    """DocString"""
+    """Recibe un argumento tipo lista que simule una matriz, y retorna su matriz transpuesta"""
     matr = [i for i in a]
     rows = len(matr)
     column = len(matr[0])
     trans = [[matr[j][i] for j in range(rows)] for i in range(column)]
     return trans
 def linealEquationsSol(a,b):
-    """DocString"""
+    """Recibe 2 argumentos de tipo lista que simulan una matriz y un vector, la matriz
+    corresponde a la matriz de un sistema de ecuaciones y el vector al vector de resultados,
+    la funcion linealEquationsSol determina la solucion del sistema de ecuaciones y lo retorna
+    en forma de lista."""
     matr = [i for i in a]
     matr_2 =[i for i in b]
     inv_matr = gaussJordan(matr )
     result = matrixProduct_2(inv_matr, matr_2)
     return result
 def determinant(*args):
-    """DocString"""
+    """Recibe como argumento una matriz y retorna su determinante."""
     if len(args) == 2 and type(args[1]) is int:
         mat = [i for i in args[0]] #
         ident = [[1 if i == j else 0 for i in range(args[1])] for j in range(args[1])]
